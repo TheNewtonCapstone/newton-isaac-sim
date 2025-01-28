691a75a484765214ed476a52e902fdbbd9696b6b
shamiivan <shamiivan@gmail.com>
2025-01-28 13:49:53 -0500

    feat: Achieved stair structure!!!!

--- core/terrain/ascending_stair_terrain.py
+++ core/terrain/ascending_stair_terrain.py
@@ -25,10 +25,10 @@ class AscendingStairsTerrainBuilder(BaseTerrainBuilder):
         self,
         size: Tensor = None,
         resolution: Tensor = None,
-        height: float = 0.05,
+        height: float = 1.0,
         root_path: str = "/Terrains",
-        stair_height: float = 0.2,
-        number_of_steps: int = 5,
+        stair_height: float = 0.1,
+        number_of_steps: int = 10,
     ):
         super().__init__(size, resolution, height, root_path)
 
@@ -53,15 +53,15 @@ class AscendingStairsTerrainBuilder(BaseTerrainBuilder):
         height=1,
         position=None,
         path=None,
-        stair_height: float = 0.20,
-        number_of_steps: int = 5,
+        stair_height: float = 0.1,
+        number_of_steps: int = 10,
     ) -> AscendingStairsTerrainBuild:
         from core.globals import TERRAINS_PATH
 
         if size is None:
             size = [10, 10]
         if resolution is None:
-            resolution = [20, 20]
+            resolution = [40, 40]
         if position is None:
             position = [0, 0, 0]
         if path is None:
@@ -73,22 +73,27 @@ class AscendingStairsTerrainBuilder(BaseTerrainBuilder):
         heightmap = torch.zeros((num_rows, num_cols))
 
         # Generate pyramid-like terrain with steps (stairs)
-        step_width = num_rows / number_of_steps
+        step_width = num_rows / (number_of_steps * 2)
 
-        platform_size = 1.0
         start_height = 0
         start_x = 0
         start_y = 0
-        stop_x = size[0]
-        stop_y = size[1]
+        stop_x = num_rows
+        stop_y = num_cols
 
-        while (stop_x - start_x) > platform_size and (stop_y - start_y) > platform_size:
+        while (stop_x > start_x) and (stop_y > start_y):
+            # print(
+            #     f"Before changes - height: {start_height}\nx: {start_x}, {stop_x}\ny: {start_y}, {stop_y}"
+            # )
             start_x = int(start_x + step_width)
             stop_x = int(stop_x - step_width)
             start_y = int(start_y + step_width)
             stop_y = int(stop_y - step_width)
             start_height += stair_height
             heightmap[start_x:stop_x, start_y:stop_y] = start_height
+            # print(
+            #     f"After changes - height: {start_height}\nx: {start_x}, {stop_x}\ny: {start_y}, {stop_y}"
+            # )
 
         # Generate pyramid-like terrain with steps (stairs)
         terrain_path = BaseTerrainBuilder._add_heightmap_to_world(
